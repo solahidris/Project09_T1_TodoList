25471a0324336387164df6276a225898067b5390
solahidris <emailsolah@gmail.com>
2023-05-10 14:08:04 +0800

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <header>
+      <div className="h-screen bg-slate-700">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p className="text-white">this is a to do list app</p>
+        <p className="text-white">this is a to do list app</p>
+        <p className="text-white">this is a to do list app</p>
+        <a href="https://reactjsexample.com/tag/todo/">sample todo link</a>
+      </div>
+    </header>
   );
 }
 
