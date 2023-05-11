ea53e1cca63211d49125f0b5fb50171f413966e8
solahidris <emailsolah@gmail.com>
2023-05-11 14:01:23 +0800

    added add list item and create map for list items

--- src/App.js
+++ src/App.js
@@ -3,47 +3,72 @@ import "./App.css";
 import { React, useState } from "react";
 
 function App() {
-  const [listInputValue, setListInputValue] = useState("");
-  const listInputValueHandler = (event) => {
-    setListInputValue(event.target.value);
+  //   const [listInputValue, setListInputValue] = useState("");
+  //   const listInputValueHandler = (event) => {
+  //     setListInputValue(event.target.value);
+  //   };
+
+  const [inputValues, setInputValues] = useState([
+    { value: "", id: Date.now() },
+    { value: "", id: Date.now() +1 },
+    { value: "", id: Date.now() +2 },
+  ]);
+
+  const handleInputChange = (e, id) => {
+    const newInputValues = inputValues.map((input) => {
+      if (input.id === id) {
+        return { ...input, value: e.target.value };
+      }
+      return input;
+    });
+    setInputValues(newInputValues);
+  };
+
+  const handleAddInput = () => {
+    setInputValues([...inputValues, { value: "", id: Date.now() }]);
   };
 
   return (
-      <div className="h-screen bg-slate-700 p-10">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="text-white text-center">this is a to do list app</p>
-          <a href="https://reactjsexample.com/tag/todo/" className="text-cyan-500 flex justify-center mb-10">sample todo link</a>
-          <p className="bg-slate-100/90 rounded px-5 py-3 mb-4 font-mono font-bold text-l text-center">Today's To-do List</p>
-          <ul>
-            <li className="list-disc">something</li>
-            <li>another li here</li>
-          </ul>
-        <div className="bg-slate-100/90 w-full rounded px-5 py-3">
-          <p className="bg-slate-100/90 rounded px-5 py-3 mb-4 font-mono font-bold uppercase text-l">Title</p>
-          <p className="px-5 py-3 font-mono text-xs">List Items</p>
-          <input type="text" className="px-5 py-3 font-mono text-xs bg-transparent"></input>
-          <ul className="list-decimal font-mono pl-10 text-xs">
-            <li><input placeholder="to do?" type="text" className="px-2 py-3 font-mono text-xs bg-transparent rounded mb-3"></input></li>
-            <li><input className={`${listInputValue === "" ? "bg-red-500" : "bg-green-500"}`} Value={listInputValue} onChange={listInputValueHandler} placeholder="to do?" type="text"></input></li>
+    <div className="h-screen bg-slate-700 p-10">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p className="text-white text-center text-xs font-mono">by solah, for solah</p>
+      <a
+        href="https://reactjsexample.com/tag/todo/"
+        className="text-cyan-500 flex justify-center mb-10"
+      >
+        sample todo link
+      </a>
+      <p className="bg-slate-100/90 rounded px-5 py-3 mb-4 font-mono font-bold text-l text-center">
+        To-do List App
+      </p>
 
-            {/* function MyComponent() {
-             const [inputValue, setInputValue] = useState('');
-            return (
-              <div>
-                <input type="text" 
-                className={`px-2 py-3 font-mono text-xs 
-                ${inputValue !== '' ? 'bg-green-500' : 'bg-slate-100/50'}`} 
-                value={inputValue} 
-                onChange={(e) => setInputValue(e.target.value)} />
-              </div>
-            );
-            } */}
+      <div className="bg-slate-100/90 w-full rounded px-5 py-3">
+        <p className="bg-slate-100/90 rounded px-5 py-3 mb-4 font-mono font-bold  text-l">
+          List up & Just do it!
+        </p>
 
-            <li><input placeholder="what do you need to do?" Value="haha" type="text" className="px-2 py-3 font-mono text-xs bg-transparent rounded mb-3"></input></li>
-            <li><input type="text" className="px-2 py-3 font-mono text-xs bg-slate-100/50 rounded mb-3"></input></li>
-          </ul>
+        <div>
+          {inputValues.map((input) => (
+            <input
+              key={input.id}
+              className={`${
+                input.value === "" ? "bg-slate-100/50" : "bg-transparent"
+              } px-2 py-3 mb-2`}
+              value={input.value}
+              onChange={(e) => handleInputChange(e, input.id)}
+              placeholder="to do?"
+              type="text"
+            />
+          ))}
         </div>
+        <button
+            onClick={handleAddInput}
+            className="px-3 py-2 mt-10 mb-3 bg-slate-100/90 hover:bg-slate-300 rounded"
+          >
+            Add to do
+          </button>
       </div>
+    </div>
   );
 }
 
