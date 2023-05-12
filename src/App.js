3a77d29bcd11803f3e6a5abf5e3a766634c8516e
solahidris <emailsolah@gmail.com>
2023-05-12 23:27:29 +0800

    added checkbox button with line-through feature to list item

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   //   };
 
   const [inputValues, setInputValues] = useState([
-    { value: "", id: Date.now() },
-    { value: "", id: Date.now() + 1 },
-    { value: "", id: Date.now() + 2 },
+    { id: 1, value: "", isChecked: false },
+    { id: 2, value: "", isChecked: false },
+    { id: 3, value: "", isChecked: false },
   ]);
 
   const handleInputChange = (e, id) => {
@@ -24,8 +24,24 @@ function App() {
     setInputValues(newInputValues);
   };
 
+  const handleCheckboxChange = (e, id) => {
+    setInputValues(
+      inputValues.map((input) =>
+        input.id === id ? { ...input, isChecked: e.target.checked } : input
+      )
+    );
+  };
+
   const handleAddInput = () => {
     setInputValues([...inputValues, { value: "", id: Date.now() }]);
+  };
+
+  const clearInputValues = () => {
+    setInputValues([
+      { value: "", id: Date.now() },
+      { value: "", id: Date.now() + 1 },
+      { value: "", id: Date.now() + 2 },
+    ]);
   };
 
   return (
@@ -34,57 +50,84 @@ function App() {
       <p className="text-white text-center text-xs font-mono">
         by solah, for solah
       </p>
-      <a
+
+      {/* SAMPLE TO DO LINK */}
+      {/* <a
         href="https://reactjsexample.com/tag/todo/"
         className="text-cyan-500 flex justify-center mb-10"
       >
         sample todo link
-      </a>
+      </a> */}
 
       <p className="bg-slate-100 italic rounded px-5 py-3 mb-4 font-mono font-bold text-l text-center">
-        "One Day at a Time" App
+        "One 💩 at a Time" App
       </p>
 
       {/* Main Container */}
       <div className="bg-slate-100/90 w-full rounded px-5 py-3">
-        {/* testing */}
-        {/* <p className="px-2 py-3 mb-2 bg-white rounded font-mono text-xs">apa ni</p>
-      <p className="px-2 py-3 mb-2 bg-white rounded font-mono text-xs">apa ni</p>
-      <div className="flex flex-col">
-        <input className="bg-red-500 mb-2" type="text"></input>
-        <input className="bg-green-300 h-auto mb-2" type="text"></input>
-      </div>
-      <div class="flex">
-        <input class=" mb-2" type="text"></input>
-        <input class=" w-auto h-full mb-2" type="text"></input>
-      </div> */}
-        {/* testing */}
-
+        {/* Sub-Title */}
         <p className="bg-slate-100/90 rounded px-5 py-3 mb-4 font-mono font-bold  text-l">
-          List up & Just do it!
+          List up & clear em up!
         </p>
 
-        <ol className="list-decimal mx-1">
-          {inputValues.map((input) => (
-            <li key={input.id}>
-              <input
-                className={`${
-                  input.value === "" ? "bg-slate-100/50" : "bg-slate-100/10"
-                } px-2 py-3 mb-2 hover:bg-slate-100/80 rounded font-mono text-xs w-auto`}
-                value={input.value}
-                onChange={(e) => handleInputChange(e, input.id)}
-                placeholder="to do?"
-                type="text"
-              />
-            </li>
-          ))}
-        </ol>
+        <div>
+          {/* List Items Generator */}
+          <ol className="list-decimal ml-1">
+            {inputValues.map((input) => (
+              <li
+                key={input.id}>
+                <input
+                  className={`${
+                    input.value === "" ? "bg-slate-100/50" : "bg-slate-100/10"
+                  } px-2 py-3 mb-2 hover:bg-slate-100/80 rounded font-mono text-xs`}
+                  value={input.value}
+                  onChange={(e) => handleInputChange(e, input.id)}
+                  placeholder="to do?"
+                  type="text"
+                  style={{
+                    textDecoration: input.isChecked ? "line-through" : "none",
+                  }}
+                />
+                <input
+                  type="checkbox"
+                  checked={input.isChecked}
+                  onChange={(e) => handleCheckboxChange(e, input.id)}
+                  className="ml-3 h-[15px] w-[15px] checked:accent-green-600 rounded"
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+
+        {/* Add list button */}
         <button
           onClick={handleAddInput}
           className="px-5 py-2 mt-10 mb-3 bg-slate-100/90 hover:bg-slate-300 rounded font-mono text-xs font-bold"
         >
           Add to do
         </button>
+
+        {/* Add Clear All button */}
+        <button
+          onClick={clearInputValues}
+          className="px-5 py-2 ml-3 mt-10 mb-3 bg-red-500/90 hover:bg-slate-300 rounded font-mono text-xs font-bold"
+        >
+          Clear All
+        </button>
+      </div>
+
+      <div className="bg-green-500">
+        <p className="my-5 p-3 bg-red-500">test</p>
+        <input button type="checkbox" />
+        <p>-------</p>
+        <label>
+          <input
+            type="checkbox"
+            className="accent-green-200 bg-blue-500"
+          ></input>{" "}
+          labels
+          <input type="checkbox" class="accent-pink-500"></input>
+        </label>
       </div>
     </div>
   );
